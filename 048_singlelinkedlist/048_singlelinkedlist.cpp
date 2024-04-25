af086ef551784eaf647378627bfe69eb1adae7af
nachool <nachma.olivia1394@gmail.com>
2024-04-25 17:14:05 +0700

    menambah switch case

--- 048_singlelinkedlist/048_singlelinkedlist.cpp
+++ 048_singlelinkedlist/048_singlelinkedlist.cpp
@@ -137,6 +137,54 @@ int main()
     {
         try
         {
+            cout << "1. Tambah data" << endl;
+            cout << "2. Hapus data" << endl;
+            cout << "3. Tampilkan data" << endl;
+            cout << "4. Cari data" << endl;
+            cout << "5. Keluar" << endl;
+            cout << "pilihan: ";
+            cin >> pilihan;
+            switch (pilihan)
+            {
+            case 1:
+                addNode();
+                cout << "Data Berhasil Ditambahkan" << endl;
+                system("pause");
+                system("cls");
+                break;
+            case 2:
+                if (listEmpty())
+                {
+                    cout << "List kosong" << endl;
+                    system("pause");
+                    system("cls");
+                    break;
+                }
+
+                int nim;
+                cout << "Masukan NIM: ";
+                cin >> nim;
+                if (deleteNode(nim)) {
+                    cout << "nim: " << nim << " berhasil dihapus" << endl;
+                    system("pause");
+                    system("cls");
+                }
+                else
+                    cout << "Data tidak ditemukan" << endl;
+                break;
+            case 3:
+                traverse();
+                break;
+            case 4:
+                searchData();
+                break;
+            case 5:
+                break;
+            default:
+                cout << "Pilihan tidak ada" << endl;
+                break;
+            }
+        }
 
         }
     }
